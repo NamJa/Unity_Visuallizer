8647d9a59753854c1a5a92a3476a6e3e0f65f221
Jongwoo Kim <kjwoo810@gmail.com>
2017-10-24 01:56:07 +0900

    fully files upload

---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using Ardunity;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    double value;
+    public AnalogInput flexAnalogInput;
+
+    void Start ()
+    {
+        
+	}
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+        value = (double)flexAnalogInput.Value * 1000;
+        value = (int)value;
+        Debug.Log(value);
+    }
+    
+}
